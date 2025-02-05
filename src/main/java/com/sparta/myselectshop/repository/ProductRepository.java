b050339e3e85acfd6d0095a09859f8b0481e3d8d
parkhg09 <parkhg09@gmail.com>
2025-02-06 01:56:27 +0900

    폴더별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.repository;
 
+import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -10,4 +11,7 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    // 쿼리메서드로는 responseDto 타입을 반환하지 못한다.
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
